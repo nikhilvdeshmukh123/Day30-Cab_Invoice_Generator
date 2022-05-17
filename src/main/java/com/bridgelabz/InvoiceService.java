b8a3f9762a9255272ed24355fd2ecd572c8b84f0
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-05-18 05:12:36 +0530

    Added: Return Invoice Using Ride Repository

---
+++ src/main/java/com/bridgelabz/InvoiceService.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+public class InvoiceService {
+    private static final int COST_PER_TIME = 1;
+    private static final double COST_PER_KM = 10;
+    private static final double MINIMUM_FARE = 5;
+    private RideRepository rideRepository;
+
+    public InvoiceService() {
+        this.rideRepository = new RideRepository();
+    }
+
+    public double calculateFare(double distance, int time) {
+        return Math.max(MINIMUM_FARE, distance * COST_PER_KM + time * COST_PER_TIME);
+    }
+
+    public InvoiceSummury calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return new InvoiceSummury(rides.length, totalFare);
+    }
+
+    public void addRides(String userId, Ride[] ride) {
+        rideRepository.addRide(userId, ride);
+    }
+
+    public InvoiceSummury getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId));
+    }
+
+}
