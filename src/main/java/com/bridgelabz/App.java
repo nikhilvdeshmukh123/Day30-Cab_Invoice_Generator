e9b22d023c44da7c8c14e47c6834665743c5ac70
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-05-18 04:55:21 +0530

    Added: Ability to calculate fare

---
+++ src/main/java/com/bridgelabz/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * @author Nikhil Deshmukh
+ * Given Distance and time, the invoice generator should return the total fare for the journey
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
